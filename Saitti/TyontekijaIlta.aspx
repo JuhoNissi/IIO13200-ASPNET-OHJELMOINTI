f32a5b12c0717517f6ff4f73d0bc0f5d5582c1aa
JuhoNissi <H9321@student.jamk.fi>
2016-09-08 18:37:46 +0300

    Demoilua 1

---
+++ Saitti/TyontekijaIlta.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TyontekijaIlta.aspx.cs" Inherits="TyontekijaIlta" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Firman SEM työntekijät</title>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="nappulat">
+        <asp:Button ID="BtnHae" CssClass="w3-btn w3-green" runat="server" Text="Hae työntekijät" OnClick="BtnHae_Click" />
+    </div>
+    <div id="esitys" class="w3-green">
+        <asp:GridView ID="gvData" runat="server"></asp:GridView>
+    </div>
+    <div id="footer" class="w3-blue-grey">
+        <asp:Label ID="lblMessage" runat="server" Text="..."></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/TyontekijaIlta.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration; //webconfigia varten
+using System.Data; //ADO.NETin varten
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class TyontekijaIlta : System.Web.UI.Page
+{
+    string xmlfilu;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //luetaan web.configista xml-tiedoston nimi
+        xmlfilu = ConfigurationManager.AppSettings["tiedosto"];
+        lblMessage.Text = xmlfilu;
+    }
+
+    protected void BtnHae_Click(object sender, EventArgs e)
+    {
+        //luetaan XML-tiedostosta työntekijöiden tiedot ja esitetään ne GritWiewssä
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        DataView dv = new DataView();
+        ds.ReadXml(Server.MapPath(xmlfilu));
+        dt = ds.Tables[0];
+        dv = dt.DefaultView;
+        //datan sitominen ui-kontrolliin
+        gvData.DataSource = dv;
+        gvData.DataBind();
+        lblMessage.Text = string.Format("Löytyi {0} työntekijää", dt.Rows.Count);
+    }
+}
